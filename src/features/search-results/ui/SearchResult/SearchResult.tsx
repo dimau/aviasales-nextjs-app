802a6e434fff3fdf92e8bf1721a2a17ec8d5aaab
Dmitrii <dimau777@gmail.com>
2023-04-16 21:06:12 +0400

    feat(main): Add logic to get and show search results

--- src/features/search-results/ui/SearchResult/SearchResult.tsx
+++ src/features/search-results/ui/SearchResult/SearchResult.tsx
@@ -1,8 +1,28 @@
 import {useAppSelector} from "../../../../app/store";
 import {selectSearchParams} from "../../../search-panel/searchParamsSlice";
+import useSWR from 'swr'
+
+const flightsEndpoint = "http://localhost:3000/api/flights";
+
+const getFlights = async (params) => {
+  const [endpoint, currency, month, origin, destination] = params;
+  const response = await fetch(`${endpoint}?currency=${currency}&month=${month}&origin=${origin}&destination=${destination}`);
+  return await response.json();
+};
 
 export default function SearchResult() {
   const searchParams = useAppSelector(selectSearchParams);
+  const key = [flightsEndpoint, searchParams.currency, searchParams.month, searchParams.origin, searchParams.destination];
+  const {data, error, isLoading} = useSWR(searchParams.currency !== "" ? key : null, getFlights);
 
-  return <p>This is a search Result: {`${searchParams.month} ${searchParams.origin} ${searchParams.destination} ${searchParams.currency}`}</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Sorry, we have an error: {JSON.stringify(error)}</p>;
+
+  return (
+      <>
+        <p>This is a search
+          Result: {`${searchParams.month} ${searchParams.origin} ${searchParams.destination} ${searchParams.currency}`}</p>;
+        <p>{data ? JSON.stringify(data) : "waiting"}</p>
+      </>
+  )
 }
--- src/pages/api/flights.ts
+++ src/pages/api/flights.ts
@@ -1,6 +1,6 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 
-export interface Flight {
+interface Flight {
   show_to_affiliates: boolean,
   trip_class: number,
   origin: string,
@@ -14,21 +14,21 @@ export interface Flight {
   actual: boolean,
 }
 
- export interface Answer {
+interface Answer {
   success: boolean;
   data?: Flight[];
 }
 
-export default async function handler(
+async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Answer>
 ) {
 
-  // Get data submitted in request's body
-  const body = req.body;
-
   // Validate incoming data and returns error if required parameters are not found
-  if (!body.currency || !body.month || !body.origin || !body.destination) {
+  if (!req.query?.currency || !req.query?.month || !req.query?.origin || !req.query?.destination) {
+    // TODO: add logging
+    console.log(`Error with query parameters. Currency: ${req.query.currency}, Month: ${req.query.month}, Origin: ${req.query.origin}, Destination: ${req.query.destination}`);
+
     // Sends a HTTP bad request error code
     return res.status(400).json({ success: false })
   }
@@ -39,18 +39,17 @@ export default async function handler(
   // Prepare URL for request to Aviasales API
   const url = new URL("http://api.travelpayouts.com/v2/prices/month-matrix");
   url.searchParams.set("token", API_KEY);
-  url.searchParams.set("currency", body.currency);
-  url.searchParams.set("month", body.month);
-  url.searchParams.set("origin", body.origin);
-  url.searchParams.set("destination", body.destination);
+  url.searchParams.set("currency", <string>req.query.currency);
+  url.searchParams.set("month", <string>req.query.month);
+  url.searchParams.set("origin", <string>req.query.origin);
+  url.searchParams.set("destination", <string>req.query.destination);
 
   // Make request to API
   const aviasalesResponse = await fetch(url);
   const aviasalesResult = await aviasalesResponse.json();
 
-  // TODO: remove
-  console.log(aviasalesResult)
-
   // Return result to client
   res.status(200).json(aviasalesResult);
 }
+
+export { Flight, Answer, handler as default};
